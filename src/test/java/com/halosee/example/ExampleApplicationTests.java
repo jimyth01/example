87130a4afea3c90aa1ec2e64b0fcc4f49765d8d0
liujiang <liujiang@halosee.com>
2022-01-26 15:25:55 +0800

    first commit

---
+++ src/test/java/com/halosee/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halosee.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
